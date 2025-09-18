b9c87edf2e558cf43da01d3f4ebf3b032b643e10
joao oliveira <xliveirvwrld@gmail.com>
2025-09-18 15:35:50 -0300

    refactor: refactor return

--- src/main/java/br/com/joao/spring_s3_qrcode_generator/repository/UserRepository.java
+++ src/main/java/br/com/joao/spring_s3_qrcode_generator/repository/UserRepository.java
@@ -2,10 +2,9 @@ package br.com.joao.spring_s3_qrcode_generator.repository;
 
 import br.com.joao.spring_s3_qrcode_generator.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmailIgnoreCase(String username);
+    Optional<User> findByEmailIgnoreCaseAndActiveTrue(String username);
 }
