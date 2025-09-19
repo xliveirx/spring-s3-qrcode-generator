00af5c89906a5dff98da755337da094cee3c861b
João Oliveira <xliveirvwrld@gmail.com>
2025-09-19 08:57:09 -0300

    chore: remove unused imports

--- src/main/java/br/com/joao/spring_s3_qrcode_generator/repository/QrCodeRepository.java
+++ src/main/java/br/com/joao/spring_s3_qrcode_generator/repository/QrCodeRepository.java
@@ -1,9 +1,12 @@
 package br.com.joao.spring_s3_qrcode_generator.repository;
 
 import br.com.joao.spring_s3_qrcode_generator.domain.QrCode;
+import br.com.joao.spring_s3_qrcode_generator.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
 
 public interface QrCodeRepository extends JpaRepository<QrCode, Long> {
     Page<QrCode> findAllByUserId(Long userId, Pageable pageable);
