d19df8c3a1bf69667986977cca962388238ff8ee
João Oliveira <xliveirvwrld@gmail.com>
2025-09-19 08:58:50 -0300

    feat: add suggested global annotation to get better pagination

--- src/main/java/br/com/joao/spring_s3_qrcode_generator/SpringS3QrcodeGeneratorApplication.java
+++ src/main/java/br/com/joao/spring_s3_qrcode_generator/SpringS3QrcodeGeneratorApplication.java
@@ -2,8 +2,12 @@ package br.com.joao.spring_s3_qrcode_generator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class SpringS3QrcodeGeneratorApplication {
 
 	public static void main(String[] args) {
