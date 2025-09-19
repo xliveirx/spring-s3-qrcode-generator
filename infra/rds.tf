015037f07c48ae236c63434c9611cdebafc2f44f
João Oliveira <xliveirvwrld@gmail.com>
2025-09-19 17:23:41 -0300

    fix: fix errors

--- infra/rds.tf
+++ infra/rds.tf
@@ -7,26 +7,9 @@ resource "aws_db_instance" "database" {
   username = var.db_username
   password = var.db_password
 
-  vpc_security_group_ids = [aws_security_group.database_sg.id]
+  # Use the default security group
+  # vpc_security_group_ids = [aws_security_group.database_sg.id]
+  skip_final_snapshot = true
   multi_az = false
   publicly_accessible = false
-}
-
-resource "aws_security_group" "database_sg" {
-  name = "database_sg"
-  description = "Security group for RDS database"
-
-  ingress {
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
--- infra/s3.tf
+++ infra/s3.tf
@@ -6,6 +6,13 @@ resource "aws_s3_bucket" "qrcodes_bucket" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "qrcodes_bucket_ownership" {
+  bucket = aws_s3_bucket.qrcodes_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "qrcodes_not_block" {
   bucket = aws_s3_bucket.qrcodes_bucket.id
   block_public_acls = false
@@ -14,7 +21,21 @@ resource "aws_s3_bucket_public_access_block" "qrcodes_not_block" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "qrcodes_allow_public-read" {
+resource "aws_s3_bucket_policy" "allow_public_read" {
   bucket = aws_s3_bucket.qrcodes_bucket.id
-  acl = "public-read"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = [
+          "${aws_s3_bucket.qrcodes_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+  depends_on = [aws_s3_bucket_public_access_block.qrcodes_not_block]
 }
