f5173e45eb020d1110954bb77d29b2333f53529e
joao oliveira <xliveirvwrld@gmail.com>
2025-09-22 15:22:12 -0300

    fix: fix error

--- app/src/main/java/br/com/joao/spring_s3_qrcode_generator/config/JwtFilterConfig.java
+++ app/src/main/java/br/com/joao/spring_s3_qrcode_generator/config/JwtFilterConfig.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -19,7 +20,7 @@ public class JwtFilterConfig extends OncePerRequestFilter {
     private final TokenService tokenService;
     private final UserRepository userRepository;
 
-    public JwtFilterConfig(TokenService tokenService, UserRepository userRepository) {
+    public JwtFilterConfig(TokenService tokenService, @Lazy UserRepository userRepository) {
         this.tokenService = tokenService;
         this.userRepository = userRepository;
     }
