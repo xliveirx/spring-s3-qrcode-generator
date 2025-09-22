30f0f149f2dd8cffddd3f6c50f1944c290605df2
joao oliveira <xliveirvwrld@gmail.com>
2025-09-22 09:30:20 -0300

    fix: fix import errors

--- app/src/main/java/br/com/joao/spring_s3_qrcode_generator/config/AwsConfig.java
+++ app/src/main/java/br/com/joao/spring_s3_qrcode_generator/config/AwsConfig.java
@@ -1,18 +1,25 @@
 package br.com.joao.spring_s3_qrcode_generator.config;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class AwsConfig {
 
     @Bean
-    public AmazonS3 amazonS3(){
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(Regions.US_EAST_1)
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(System.getenv("AWS_REGION")))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(System.getenv("AWS_REGION")))
                 .build();
     }
 }
--- app/src/main/java/br/com/joao/spring_s3_qrcode_generator/service/S3Service.java
+++ app/src/main/java/br/com/joao/spring_s3_qrcode_generator/service/S3Service.java
@@ -8,7 +8,7 @@ import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetUrlRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
 import java.io.ByteArrayInputStream;
 import java.net.URL;
