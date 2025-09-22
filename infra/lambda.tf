d6a90ba7b182fde95f68019b654df1f5f6f44269
joao oliveira <xliveirvwrld@gmail.com>
2025-09-22 10:49:40 -0300

    fix: fix errors

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -51,8 +51,8 @@ resource "aws_iam_policy_attachment" "attach" {
 resource "aws_s3_object" "lambda_package" {
   bucket = var.lambda_bucket_name
   key    = "lambda/prod/lambda.zip"
-  source = "${path.module}/../lambda.zip"
-  # etag   = filemd5("${path.module}/../lambda.zip")
+  source = "${path.module}/../app/target/lambda.zip"
+  # etag   = filemd5("${path.module}/../app/target/lambda.zip")
 }
 
 resource "aws_lambda_function" "lambda" {
